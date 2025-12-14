b0327c790032868f91ea18ed607c8aa435c677ba
Rabramxv <krotan152@gmail.com>
2025-12-14 18:27:49 +0300

    ok

--- _next/static/wS59Ed_3D0dhTXbmNwDCJ/_buildManifest.js
+++ _next/static/wS59Ed_3D0dhTXbmNwDCJ/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Rabramxv.github.io//_next/:path+",
-        "destination": "/Rabramxv.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
